08de896870bcf466884e02167de73358697287b9
MalluruMamatha <mamatha.mm939@gmail.com>
2024-09-17 16:28:13 +0530

    k8

--- installation.sh
+++ installation.sh
@@ -40,11 +40,20 @@ VALIDATE $? "Docker installation"
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
-VALIDATE $? "eksctl installation"
+if [ $? -ne 0 ]; then
+  echo "Error: eksctl installation failed"
+else
+  echo "eksctl installation succeeded"
+fi
 
 
 # kubectl
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.0/2024-05-12/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv kubectl /usr/local/bin/kubectl
-VALIDATE $? "kubectl installation"
+kubectl version --client
+if [ $? -ne 0 ]; then
+  echo "Error: kubectl installation failed"
+else
+  echo "kubectl installation succeeded"
+fi
